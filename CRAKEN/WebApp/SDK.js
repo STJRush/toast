6bba7d8ca3c22721d6486eafb493cf12dd6a6fcb
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2020-12-03 09:30:03 +0000

    Create SDK.js

---
+++ CRAKEN/WebApp/SDK.js
@@ -0,0 +1,24 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDG2omp8W3ImCd7EW1-QGe12zzOiQLXIBU",
+    authDomain: "c-r-a-k-e-n.firebaseapp.com",
+    databaseURL: "https://c-r-a-k-e-n.firebaseio.com",
+    projectId: "c-r-a-k-e-n",
+    storageBucket: "c-r-a-k-e-n.appspot.com",
+    messagingSenderId: "367386643937",
+    appId: "1:367386643937:web:b8f998fcbb9e0c0e7656a0",
+    measurementId: "G-KTDMRH0CNF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
