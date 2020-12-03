0eb39ac7265981d428c4665d6ddaa9d48af36764
Stephen Flynn <25164860+syntaxerrorStephen@users.noreply.github.com>
2020-12-03 09:28:25 +0000

    Delete main.py

--- CRAKEN/main.py
+++
@@ -1,9 +0,0 @@
-#CRKAEN OpenCV Processing Test
-import cv2 
-import pytesseract
-
-img = cv2.imread('image.jpg')
-
-# Adding custom options
-custom_config = r'--oem 3 --psm 6'
-pytesseract.image_to_string(img, config=custom_config)
